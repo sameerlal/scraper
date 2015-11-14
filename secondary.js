f9f2118f615937e43d43bfdaa7d41f22a1916559
Sameer Lal <sj_lal@hotmail.com>
2015-11-13 20:29:10 -0500

    WE WILL WIN

--- secondary.js
+++ secondary.js
@@ -1,6 +1,15 @@
- 
+function logarithm(){
+} 
+function SAMEERBot(){
+	var i = 0;
+	while(i<100){
+	var hi = document.getElementsByName("yop_poll_answer[2]");
+	hi[6].checked=true;
+	var button = document.getElementsByClassName("yop_poll_vote_button")[0].click();
+	location.reload();
+	i++
+	window.setTimeout(logarithm, 20);
+	}
+}
+SAMEERBot();
 
-var hi = document.getElementsByName("yop_poll_answer[2]");
-hi[6].checked=true;
-var button = document.getElementsByClassName("yop_poll_vote_button")[0].click();
- 
