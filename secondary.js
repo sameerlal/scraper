833c33191cf8028202cac82c0e70ef875cff43ca
Sameer Lal <sj_lal@hotmail.com>
2015-11-16 18:16:44 -0500

    version 2 update

--- secondary.js
+++ secondary.js
@@ -1,7 +1,9 @@
-
+/* Version 2
+*  Set a timeout and console log.
+*/
 function logarithm(){
 } 
-function theBot(){
+function EmmausBot(){
 	var i = 0;
 	while(i<100){
 	var hi = document.getElementsByName("yop_poll_answer[2]");
@@ -9,9 +11,10 @@ function theBot(){
 	var button = document.getElementsByClassName("yop_poll_vote_button")[0].click();
 	location.reload();
 	i++
+	console.log("Sent" + i + "th request to server.");
 	// Errors encountered at 20 ms delay. Sent 3% of requests to server.
 	window.setTimeout(logarithm, 30);
 	}
 }
-theBot();
+EmmausBot();
 
