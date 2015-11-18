42656bef4c9a6e22657cd21f2309194560376b0a
Sameer Lal <sj_lal@hotmail.com>
2015-11-17 19:42:24 -0500

    added secondary--doesnt work recursive depth O(n^k)--works with small exponents

--- oldbot.js
+++ oldbot.js
@@ -1,20 +1,22 @@
 /* Version 2
 *  Set a timeout and console log.
 */
-
+function logarithm(){
+	console.log("...");
+} 
 function EmmausBot(){
 	var i = 0;
 	while(i<100000){
 	var hi = document.getElementsByName("yop_poll_answer[2]");
 	hi[6].checked=true;
 	var button = document.getElementsByClassName("yop_poll_vote_button")[0].click();
-	window.setTimeout(EmmausBot(),5000);
+	window.setTimeout(logarithm,2000);
 	console.log("clicked button...");
 	location.reload();
 	i++
 	console.log("> Sent" + i + "th request to server.");
 	// Errors encountered at 20 ms delay. Sent 3% of requests to server.
-	window.setTimeout(EmmausBot(), 1000);
+	window.setTimeout(logarithm, 1000);
 	}
 }
 EmmausBot();
