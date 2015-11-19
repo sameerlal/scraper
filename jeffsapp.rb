a09e24e71b103a6d7b4ac6a1d43cde6355192601
Sameer Lal <sj_lal@hotmail.com>
2015-11-18 20:29:56 -0500

    comiples

--- jeffsapp.rb
+++ jeffsapp.rb
@@ -1,10 +1,14 @@
 require 'mechanize'
 require 'pp'
 m = Mechanize.new
-p = m.get("http://www.freefalltrampolinepark.com/poll/")
-form = p.forms.first
-# value: "6" for EHS
-pp form
-form.radiobutton_with(value: "6").check
-sleep(1.0/24.0)
-form.submit
+
+for i in 0..10000
+	p = m.get("http://www.freefalltrampolinepark.com/poll/")
+	form = p.forms.first
+	# value: "6" for EHS
+	# pp form
+	form.radiobutton_with(value: "6").check
+	sleep(1)
+	form.submit
+	puts "done " + i.to_s 
+end
