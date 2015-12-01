9d35ba5eb4d3e173b58447ce53d500ed4158b85b
Sameer Lal <sj_lal@hotmail.com>
2015-12-01 18:00:08 -0500

    Last commit

--- jeffsapp.rb
+++ jeffsapp.rb
@@ -1,14 +1,14 @@
 require 'mechanize'
-require 'pp'
 m = Mechanize.new
+
 
 for i in 0..10000
 	p = m.get("http://www.freefalltrampolinepark.com/poll/")
 	form = p.forms.first
 	# value: "6" for EHS
 	# pp form
-	form.radiobutton_with(value: "6").check
+	form.radiobutton_with(value: "5").check
 	sleep(1)
-	form.submit
+	form.click_button()
 	puts "done " + i.to_s 
 end
